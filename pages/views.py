c6b9b9178189b301b2cd8627264d9c0e574bbd20
Ubuntu <ubuntu@ip-172-31-87-64.ec2.internal>
2019-08-30 12:27:03 +0000

    added login authorisation, updated base for template inheritance

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from pages.forms import UserLoginForm
 
 # Create your views here.
@@ -15,6 +16,7 @@ def about_page(request):
     #bring user to about page
     return render (request, "about.html")
     
+@login_required
 def logout(request):
     #log user out
     auth.logout(request)
@@ -23,5 +25,23 @@ def logout(request):
     
 def login(request):
     """Show login page"""
-    login_form = UserLoginForm()
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in.")
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Your Username or Password is incorrect!")
+                
+    else:
+        login_form = UserLoginForm()
+        
     return render(request, 'login.html', {"login_form":login_form})
