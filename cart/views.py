d63b9dda19653f475828c78a6919637a8849d2d3
Ubuntu <ubuntu@ip-172-31-87-64.ec2.internal>
2019-09-05 08:18:32 +0000

    commented out if else statement on databases in settings, changed add_to_cart view to redirect to cart instead of index

--- cart/views.py
+++ cart/views.py
@@ -20,7 +20,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('view_cart'))
 
 
 def adjust_cart(request, id):
