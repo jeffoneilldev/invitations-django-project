248599c02d136eea7f592143a8470d738421d2d6
Ubuntu <ubuntu@ip-172-31-87-64.ec2.internal>
2019-09-06 11:07:20 +0000

    added env.py file with a new secret key and put into gitignore

--- invitations/settings.py
+++ invitations/settings.py
@@ -85,22 +85,19 @@ WSGI_APPLICATION = 'invitations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# if "DATABASE_URL" in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     print("Postgres URL not found, using sqlite instead")
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
