fd9696a8e9cd40e89013248b1fa7130ccdfb773d
Ubuntu <ubuntu@ip-172-31-87-64.ec2.internal>
2019-09-15 11:01:52 +0000

    fixed page urls to display only the selected page url

--- invitations/urls.py
+++ invitations/urls.py
@@ -27,7 +27,7 @@ from .settings import MEDIA_ROOT
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
-    url(r'^pages/', include(urls_pages)),
+    url(r'^', include(urls_pages)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
--- pages/urls.py
+++ pages/urls.py
@@ -3,10 +3,10 @@ from pages.views import index, about_page, logout, login, registration, customer
 
 urlpatterns = [
     url(r'^$', index, name="index"),
-    url(r'^about$', about_page, name="about_page"),
-    url(r'^pages/logout/$', logout, name="logout"),
-    url(r'^pages/login/$', login, name="login"),
-    url(r'^pages/register/$', registration, name="registration"),
-    url(r'^accounts/profile/$', customer_profile, name="profile")
+    url(r'^about/$', about_page, name="about_page"),
+    url(r'^logout/$', logout, name="logout"),
+    url(r'^login/$', login, name="login"),
+    url(r'^register/$', registration, name="registration"),
+    url(r'^profile/$', customer_profile, name="profile")
 ]
 
